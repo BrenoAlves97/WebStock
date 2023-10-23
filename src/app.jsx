04def548a6950345a4d097f8ba7fdbae81f8827e
Breno Alves <breno_ja@hotmail.com>
2023-10-22 22:02:23 -0300

    add react toast

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { Toaster } from 'react-hot-toast';
+
 import { Home } from './pages/home/home.page.jsx';
 import { Products } from './pages/products/products.page.jsx';
 
@@ -10,6 +12,18 @@ export const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            className: '',
+            style: {
+              border: '1px solid #092551',
+              padding: '4px',
+              color: '#092551',
+              fontSize: '12px',
+            },
+          }}
+        />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
