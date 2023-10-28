b6914c8591268a8afc540cd45b46296898f8b565
Breno Alves <breno_ja@hotmail.com>
2023-10-27 21:36:42 -0300

    add disabled property to button and change message font-size

--- src/components/form/custom-button/custom-button.component.jsx
+++ src/components/form/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import styles from './custom-button.module.css';
 
-export const CustomButton = ({ children, onClick }) => {
+export const CustomButton = ({ children, onClick, ...props }) => {
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button onClick={onClick} className={styles.button} {...props}>
       {children}
     </button>
   );
