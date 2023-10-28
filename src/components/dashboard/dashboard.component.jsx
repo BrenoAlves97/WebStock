9b9eaa9ee1b436af11658cfc96d01f2835f731a2
Breno Alves <breno_ja@hotmail.com>
2023-10-28 17:07:58 -0300

    delete item add to dashboard

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { FiTrash, FiEdit2 } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
+import { FiTrash, FiEdit2 } from 'react-icons/fi';
+import toast from 'react-hot-toast';
 
-import { getDocs, collection, query } from 'firebase/firestore';
+import { doc, deleteDoc, getDocs, collection, query, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase/firebase.app.js';
 
 import styles from './dashboard.module.css';
 
 export const Dashboard = () => {
-  const [products, setProducts] = React.useState(null);
+  const [products, setProducts] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
 
   const navigate = useNavigate();
@@ -18,31 +19,31 @@ export const Dashboard = () => {
     currency: 'BRL',
   });
 
-  React.useEffect(() => {
-    const fetchProducts = async () => {
-      setLoading(true);
-      try {
-        const querySnapshot = await getDocs(collection(db, '@products'));
-        let arrData = [];
-        querySnapshot.forEach((doc) => {
-          arrData.push({
-            id: doc.data().id,
-            name: doc.data().name,
-            category: doc.data().category,
-            price: doc.data().price.replace(',', '.'),
-            quantity: doc.data().quantity,
-            sales: doc.data().sales,
-          });
-
-          setProducts(arrData);
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const querySnapshot = await getDocs(collection(db, '@products'));
+      let arrData = [];
+      querySnapshot.forEach((doc) => {
+        arrData.push({
+          id: doc.data().id,
+          name: doc.data().name,
+          category: doc.data().category,
+          price: doc.data().price.replace(',', '.'),
+          quantity: doc.data().quantity,
+          sales: doc.data().sales,
         });
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
 
+        setProducts(arrData);
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
     fetchProducts();
   }, []);
 
@@ -50,14 +51,24 @@ export const Dashboard = () => {
     navigate(`/editar/${productId}`);
   };
 
-  const handleRemoveItem = async (productId) => {};
+  const handleRemoveItem = async (productId) => {
+    await deleteDoc(doc(db, '@products', String(productId)))
+      .then(async () => {
+        toast.success('Produto removido de sua lista');
+        return await fetchProducts();
+      })
+      .catch((err) => {
+        return toast.error('Houve algum erro...');
+      });
+  };
 
   if (loading) return <h2 className={`container ${styles.loading_message}`}>Buscando produtos...</h2>;
 
-  if (!products) return <h2 className={`container ${styles.error_message}`}>Nenhum produto encontrado...</h2>;
+  if (products.length === 0)
+    return <h2 className={`container ${styles.error_message}`}>Nenhum produto encontrado...</h2>;
 
   return (
-    products &&
+    products.length > 0 &&
     !loading && (
       <div className={styles.category_content}>
         <table className={styles.table}>
@@ -82,7 +93,12 @@ export const Dashboard = () => {
                 <td>{item.sales}</td>
                 <td>
                   <span>
-                    <FiTrash size={18} color="#141414" className={styles.btn_remove} />
+                    <FiTrash
+                      size={18}
+                      color="#141414"
+                      className={styles.btn_remove}
+                      onClick={() => handleRemoveItem(item.id)}
+                    />
                     <FiEdit2
                       size={18}
                       color="#141414"
