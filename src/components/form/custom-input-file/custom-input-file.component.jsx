0677bfe9597c45367a518f1f39f6bd250d4f9995
Breno Alves <breno_ja@hotmail.com>
2023-11-17 23:04:01 -0300

    install uuid, and custom-input-file component modified

--- src/components/form/custom-input-file/custom-input-file.component.jsx
+++ src/components/form/custom-input-file/custom-input-file.component.jsx
@@ -2,24 +2,7 @@ import React from 'react';
 
 import Logo from '../../../assets/img_croche.jpg';
 
-export const CustomInputFile = ({ children, id, value, setValue, setImgDB, ...props }) => {
-  const [image, setImage] = React.useState(null);
-
-  const handleChange = (e) => {
-    const result = e.target.files[0];
-
-    if (result) {
-      if (result.type === 'image/jpeg' || result.type === 'image/png') {
-        setImgDB(result);
-        const img = URL.createObjectURL(result);
-        setValue(img);
-      }
-    } else {
-      setValue(null);
-      return;
-    }
-  };
-
+export const CustomInputFile = ({ children, id, handleFile, ...props }) => {
   return (
     <div className="flex items-center justify-center relative rounded-full duration-75 border-2 border-transparent hover:border-gray-500/80">
       <button className="cursor-pointer flex items-center justify-center absolute">
@@ -27,14 +10,10 @@ export const CustomInputFile = ({ children, id, value, setValue, setImgDB, ...pr
           <span className="cursor-pointer">{children}</span>
         </div>
         <div className="cursor-pointer w-full relative items-center flex justify-center group">
-          <input onChange={handleChange} {...props} className="opacity-0 cursor-pointer absolute" />
+          <input onChange={(event) => handleFile(event)} {...props} className="opacity-0 cursor-pointer absolute" />
         </div>
       </button>
-      <img
-        className="w-52 h-52 rounded-full object-cover object-center"
-        src={value ? value : Logo}
-        alt="Imagem Inicial de Crochê"
-      />
+      <img className="w-52 h-52 rounded-full object-cover object-center" src={Logo} alt="Imagem Inicial de Crochê" />
     </div>
   );
 };
