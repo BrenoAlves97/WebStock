4b066f0e47026e16121270e5f3820f156cd5dc87
Breno Alves <breno_ja@hotmail.com>
2023-11-22 18:16:52 -0300

    container add on loading return

--- src/components/private/private.component.jsx
+++ src/components/private/private.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/auth-context.jsx';
 
+import { Container } from '../container/container.component.jsx';
+
 export const Private = ({ children }) => {
    const { signed, loadingAuth } = React.useContext(AuthContext);
 
-   if (loadingAuth) return <div></div>;
+   if (loadingAuth) return <Container />;
 
    if (!signed) return <Navigate to="/login" />;
 
